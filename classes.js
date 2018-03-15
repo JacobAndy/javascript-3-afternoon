2977108d75c0effe523e44457f5f538aa480f4d5
Jacob Anderson <jacob.andy@Jacobs-MacBook-Pro.local>
2018-03-14 20:44:12 -0500

    initial commit

--- classes.js
+++ classes.js
@@ -29,9 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
-
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -49,9 +57,18 @@
   Call your new class Manager
 */
 
-//Code Here
-
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age, reports);
+    this.reports = [];
+  }
+  hire(employee) {
+    this.reports.push(employee);
+  }
+  fire(index) {
+    this.reports.splice(index, 1);
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -74,12 +91,36 @@
 
   Call your new class ProgressiveManager
 */
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, title, bonus) {
+    super(first_name, last_name, email, age, reports);
+    this.title = "Not a manager";
+    this.bonus = 0;
+  }
+  hire(employee) {
+    this.reports.push(employee);
+    var repL = this.reports.length;
+    if (repL > 100) {
+      this.title = "Bestest Manager";
+    } else if (repL > 50 && repL < 101) {
+      this.title = "Manager Plus";
+    } else if (repL > 10 && repL < 51) {
+      this.title = "Manager";
+    } else if (repL > 3 && repL < 11) {
+      this.title = "Mostly Manager";
+    } else if (repL > 0 && repL < 4) {
+      this.title = "Barely Manager";
+    } else {
+      this.title = "Not a manager";
+    }
+  }
+  fire(index) {
+    this.reports.splice(index, 1);
+    this.bonus += 100;
+  }
+}
 
-//Code Here
-
-
-
-////////// PROBLEM 4 - Black Diamond //////////
+//////// PROBLEM 4 - Black Diamond //////////
 
 /*
   Widget Co has a factory that makes widgets.
@@ -102,6 +143,23 @@
         - It should set decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
-
-
+class Machine {
+  constructor() {
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
+  makeWidgets(num) {
+    this.widgets_made_count += num;
+    this.wear_and_tear_count += Math.floor(num / 50);
+  }
+  fixMachine() {
+    this.needs_reboot = true;
+  }
+  reboot() {
+    return () => {
+      this.wear_and_tear_count -= 10;
+      this.needs_reboot = false;
+    };
+  }
+}
